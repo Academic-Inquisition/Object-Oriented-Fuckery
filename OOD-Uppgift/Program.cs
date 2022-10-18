7667d3cf0ff98027870ba5f1cddd552a9ccb6a93
Lanse <simon_stalnabb@hotmail.com>
2022-10-18 17:49:50 +0200

    Init

---
+++ OOD-Uppgift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOD_Uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
