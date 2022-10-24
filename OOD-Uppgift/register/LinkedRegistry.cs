2c8204ad511132f84b63713741f2740879df80ae
Nisslett <Nisslett@users.noreply.github.com>
2022-10-24 20:23:46 +0200

    LinkedRegistry + .gitignore
    LinkedRegistry funkar nu. Jag städade också upp lite i projektet så att den inte tar med filler som vi inte behöver. Imed en .girignore fil plus att jag tog bort mapparna obj ,bin och .vs .

--- OOD-Uppgift/register/LinkedRegistry.cs
+++ OOD-Uppgift/register/LinkedRegistry.cs
@@ -1,6 +1,8 @@
 ﻿using OOD_Uppgift.employee;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,46 +13,102 @@ namespace OOD_Uppgift.register
     {
         private static DictionaryRegistry? registry;
 
+        private LinkedList<KeyValuePair<string,Employee>> _linkedRegistry;
+
         public LinkedRegistry()
         {
-            // Stub
-            // TODO: Implement LinkedList Version of Registry
+            _linkedRegistry= new LinkedList<KeyValuePair<string,Employee>>();
         }
 
         public override bool Add(string socialId, Employee employee)
         {
-            throw new NotImplementedException();
+            //Does the socialId already exist?
+            if (GetKeyValuePair(socialId)!=null)
+            {
+                Console.WriteLine($"Error: Cannot add new Employee with Social ID: {socialId}, because said employee already exists!");
+                return false;
+            }
+            _linkedRegistry.AddLast(new KeyValuePair<string,Employee>(socialId, employee));
+            return true;
         }
 
-        public override Employee Get(string socialId)
+        
+        public override Employee? Get(string socialId)
         {
-            throw new NotImplementedException();
-        }
-
-        public override bool Remove(Employee employee)
-        {
-            throw new NotImplementedException();
+            var kvp = GetKeyValuePair(socialId);
+            if (kvp!=null)
+            {
+                return  kvp.Value.Value;
+            }
+            Console.WriteLine($"Error: Cannot get Employee with Social ID: {socialId}, because said employee doesn't exist!");
+            return null;
         }
 
         public override bool Remove(string socialId)
         {
-            throw new NotImplementedException();
+            var kvp = GetKeyValuePair(socialId);
+            if (kvp != null)
+            {
+                return _linkedRegistry.Remove(kvp.Value);
+            }
+            return false;
+            
+
         }
 
-        public override bool RemoveAll()
+        public override void RemoveAll()
         {
-            throw new NotImplementedException();
+            _linkedRegistry.Clear();
         }
 
-        public override Employee Update(string socialId, Employee employee)
+        public override Employee? Update(string socialId, Employee employee)
         {
-            throw new NotImplementedException();
+
+            if (this.Remove(socialId))
+            {
+                return this.Add(socialId, employee) ? employee : null;
+            }
+            Console.WriteLine($"Error: Failed to Update Employee with Social ID: {socialId}");
+            return null;
         }
 
-        public override Employee Update(string socialId, Func<Employee, Employee> updateFunction)
+        public override Employee? Update(string socialId, Func<Employee, Employee> updateFunction)
         {
-            throw new NotImplementedException();
+
+            var kvp = GetKeyValuePair(socialId);
+            if (kvp != null)
+            { 
+                Employee employee = kvp.Value.Value;
+                if (this.Remove(socialId)==true)
+                {
+                    updateFunction(employee);
+                    return this.Add(socialId, employee) ? employee : null;
+                }
+                Console.WriteLine($"Error: Failed to Update Employee with Social ID: {socialId}");
+            }
+            return null;
         }
+
+        //---------------------------------------
+
+        //Helper Function
+        private KeyValuePair<string, Employee>? GetKeyValuePair(string socialId)
+        {
+            LinkedListNode<KeyValuePair<string, Employee>>? node = _linkedRegistry.First;
+            while (node != null)
+            {
+                //Check if the current node has the socialId we are looking for
+                if (node.Value.Key == socialId)
+                {
+                    // if yes then return the employee
+                    return node.Value;
+                }
+                node = node.Next;
+            }
+            return null;
+        }
+
+        //----------------------------------------
 
         public static BaseRegistry GetRegistry()
         {
